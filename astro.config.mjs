cacf9a3ee0a699dcb7de6ab1f979bc52d96a84e5
m.antonovich <m.antonovich@thirdpin.io>
2025-03-10 14:51:25 +0300

    [github] remove extra folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://github.com/RiByryn',
-	outDir: 'public',
-	publicDir: 'static',
 	integrations: [
 		starlight({
 			title: 'My Docs',
